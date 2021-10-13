3ddd7f33dc2b857820b97d660f5e54d6c56c478d
alivgalihpp <alivgalih12@gmail.com>
2021-10-13 19:49:04 +0700

    p4

---
+++ problems/Assets/Problem4/p4BallController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class p4BallController : MonoBehaviour
+{
+    // Rigidbody 2D bola
+    private Rigidbody2D rigidBody2D;
+    public float velocity = 5f;
+
+     void ResetBall()
+    {
+        // Reset posisi menjadi (0,0)
+        transform.position = Vector2.zero;
+ 
+        // Reset kecepatan menjadi (0,0)
+        rigidBody2D.velocity = Vector2.zero;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {	
+		Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		rigidBody2D.MovePosition(rigidBody2D.position + (input * velocity * Time.fixedDeltaTime));
+	}
+}
